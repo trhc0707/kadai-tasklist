21a51525317f75f9cb05d5950f23145c019a253b
takuma.imamura <trhc0707@icloud.com>
2019-04-03 07:59:22 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     
-    before_action :require_user_logged_in, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
 
     def index
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
@@ -59,5 +60,12 @@ class TasksController < ApplicationController
     
     def task_params
         params.require(:task).permit(:content, :status)
-end
+    end
+    
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
+    end 
 end
